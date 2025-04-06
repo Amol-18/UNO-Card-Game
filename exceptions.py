a6958c6d42b0f84a39d9bad78930328c2b41e715
Amol-18 <amolkatke10@gmail.com>
2025-04-06 17:21:04 +0530

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+class InvalidMoveException(Exception):
+    """Raised when an invalid move is attempted."""
+    pass
+
+class GameRuleException(Exception):
+    """Raised when a game rule is violated."""
+    pass
